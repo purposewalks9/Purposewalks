53f4a1e784da35c1965b238d4b46ed3315c60403
purposewalks9 <boypee71@gmail.com>
2026-08-24 21:05:48 -0400

    done'

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,11 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import { ThemeProvider } from "@/components/theme-provider";
+import type { Metadata } from "next";
+import {
+  DynaPuff,
+  Geist,
+  Geist_Mono,
+  Instrument_Serif,
+} from "next/font/google";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -13,20 +18,39 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 
+const instrumentSerif = Instrument_Serif({
+  variable: "--font-instrument-serif",
+  subsets: ["latin"],
+  weight: ["400"],
+});
+
+const dynaPuff = DynaPuff({
+  variable: "--font-dyna-puff",
+  subsets: ["latin"],
+});
+
 export const metadata: Metadata = {
   title: "Purpose",
   description: "Full-stack Engineer",
 };
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+
+export default async function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
   return (
-    <html
-      lang="en"
-      suppressHydrationWarning
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
-    >
-      <body className="min-h-full flex flex-col">
-        <ThemeProvider
+    <html lang="en" suppressHydrationWarning data-scroll-behavior="smooth">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} ${instrumentSerif.variable} ${dynaPuff.variable} min-h-dvh bg-background text-foreground antialiased font-sans`}
+        suppressHydrationWarning
+        style={{
+          "--font-sans": "var(--font-geist-sans)",
+          "--font-mono": "var(--font-geist-mono)",
+        } as React.CSSProperties}
+      >
+           <ThemeProvider
           attribute="class"
           defaultTheme="dark"
           enableSystem
